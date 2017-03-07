6dc0cd8c8b2993ae690ab031ca044ca3b62078c4
Sahib Khan <sk213d@att.com>
2017-03-07 17:04:07 -0500

    Added Sidebar

--- views/_partials/sidebar.php
+++ views/_partials/sidebar.php
@@ -1,14 +1,49 @@
-<div class="sidebar-nav">
-    <div class="well" style="width:300px; padding: 8px 0;">
-		<ul class="nav nav-list"> 
-		  <li class="nav-header">Admin Menu</li>        
-		  <li><a href="index"><i class="icon-home"></i> Dashboard</a></li>
-          <li><a href="#"><i class="icon-envelope"></i> Messages <span class="badge badge-info">4</span></a></li>
-          <li><a href="#"><i class="icon-comment"></i> Comments <span class="badge badge-info">10</span></a></li>
-		  <li class="active"><a href="#"><i class="icon-user"></i> Members</a></li>
-          <li class="divider"></li>
-		  <li><a href="#"><i class="icon-comment"></i> Settings</a></li>
-		  <li><a href="#"><i class="icon-share"></i> Logout</a></li>
-		</ul>
+<nav class="navbar navbar-default sidebar" role="navigation">
+    <div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-sidebar-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">Brand</a>
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Home<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
+				<li ><a href="#">Profile<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
+				<li ><a href="#">Messages<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-envelope"></span></a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a>
+					<ul class="dropdown-menu forAnimate" role="menu">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Something else here</a></li>
+						<li class="divider"></li>
+						<li><a href="#">Separated link</a></li>
+						<li class="divider"></li>
+						<li><a href="#">One more separated link</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Home<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
+				<li ><a href="#">Profile<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
+				<li ><a href="#">Messages<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-envelope"></span></a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a>
+					<ul class="dropdown-menu forAnimate" role="menu">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Something else here</a></li>
+						<li class="divider"></li>
+						<li><a href="#">Separated link</a></li>
+						<li class="divider"></li>
+						<li><a href="#">One more separated link</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
 	</div>
-</div>
+</nav>
--- views/_partials/sidebar_layout.php
+++ views/_partials/sidebar_layout.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <?php 
   $this->load_partial("views/_partials/head");?>
+  <link rel="stylesheet" type="text/css" href="<?php echo $this->root_url()?>/assets/css/sidebar.css">
 <html>
 <body>
 	<?php 
 	 $this->load_partial("views/_partials/nav");
   	 $this->load_partial("views/_partials/sidebar");?>
-	<div class="home">
-		<div class="tickets-list">
-			{{body}}
-		</div>
+	<div class="main">
+		{{body}}
 	</div>
 	<?php $this->load_partial("views/_partials/footer"); ?>
+	<script type="text/javascript" src="<?php echo $this->root_url()?>/assets/js/sidebar.js"></script>
 </body>
 </html>
