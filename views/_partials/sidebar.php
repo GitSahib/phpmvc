81587bc0a729d0dea64abf1a899661fdf052ac65
Sahib Khan <sk213d@att.com>
2017-03-08 13:27:35 -0500

    Added tqi link

--- views/_partials/sidebar.php
+++ views/_partials/sidebar.php
@@ -16,18 +16,8 @@
 				<li class="active"><a href="<?php echo $this->root_url();?>">Home<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
 				<li ><a href="/teamdb">TeamDB<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
 				<li ><a href="/Templates/cgi-bin/Template.pl">Template Tool<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-envelope"></span></a></li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a>
-					<ul class="dropdown-menu forAnimate" role="menu">
-						<li><a href="#">Action</a></li>
-						<li><a href="#">Another action</a></li>
-						<li><a href="#">Something else here</a></li>
-						<li class="divider"></li>
-						<li><a href="#">Separated link</a></li>
-						<li class="divider"></li>
-						<li><a href="#">One more separated link</a></li>
-					</ul>
-				</li>
+				<li ><a href="/tqi3/cgi-bin/index.pl">TQI<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-envelope"></span></a></li>
+				
 				<li><a href="http://m5devacoe01.gcsc.att.com:23178/shift/">TeamDB_API<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
 				<li ><a href="https://icode3.web.att.com/cru?filter=inbox">ICODE<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
 				<li ><a href="https://itrack.web.att.com/issues/?filter=-1">ITRACK<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-envelope"></span></a></li>
